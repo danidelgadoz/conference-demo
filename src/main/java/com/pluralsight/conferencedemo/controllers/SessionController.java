17ee918cb0908c66b05e7bee8cbfe2a1a348592f
Dandel <dedd1993@gmail.com>
2020-07-19 20:49:19 -0500

    Demo: Creating Spring API REST Controllers - Part 2

--- src/main/java/com/pluralsight/conferencedemo/controllers/SessionController.java
+++ src/main/java/com/pluralsight/conferencedemo/controllers/SessionController.java
@@ -2,6 +2,7 @@ package com.pluralsight.conferencedemo.controllers;
 
 import com.pluralsight.conferencedemo.models.Session;
 import com.pluralsight.conferencedemo.repositories.SessionRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,5 +24,23 @@ public class SessionController {
     public Session get(@PathVariable Long id) {
         return sessionRepository.getOne(id);
     }
-    
+
+    @PostMapping
+    public Session create(@RequestBody final Session session) {
+        return sessionRepository.saveAndFlush(session);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id) {
+        // Also need to check for children records before deleting.
+        sessionRepository.deleteById(id);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    public Session update(@PathVariable Long id, @RequestBody Session session) {
+        // because this is a PUT, we expect all attributes to be passed in. A PATH would only need what is intended to update.
+        Session existingSession = sessionRepository.getOne(id);
+        BeanUtils.copyProperties(session, existingSession, "session_id");
+        return sessionRepository.saveAndFlush(existingSession);
+    }
 }
